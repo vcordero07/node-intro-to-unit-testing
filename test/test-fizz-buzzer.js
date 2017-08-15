0fe2a1e89de2f8007587378f0bf5f20f765fa181
vcordero07 <vcordero07@gmail.com>
2017-08-14 23:28:54 -0400

    wip-refactoring

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -4,17 +4,35 @@ const fizzBuzzer = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function() {
 
+  it('should return "fizz-buzz" for multiples of 15', function() {
+    [15, 30, 45].forEach(function(input) {
+      fizzBuzzer(input).should.equal('fizz-buzz');
+    });
+  });
 
+  it('should return "fizz" for multiples of 3', function() {
+    [3, 6, 9, 12].forEach(function(input) {
+      fizzBuzzer(input).should.equal('fizz');
+    });
+  });
 
-  it ('should raise error if args not numbers', function() {
-    const badInputs = [
-      ['a',1],
-      ['1',2],
-      [2, false]
-    ];
+  it('should return "buzz" for multiples of 5', function() {
+    [5, 10, 20].forEach(function(input) {
+      fizzBuzzer(input).should.equal('buzz');
+    });
+  });
+
+  it('should return number if not mult of 3 or 5', function() {
+    [1, 2, 4, 7].forEach(function(input) {
+      fizzBuzzer(input).should.equal(input);
+    });
+  });
+
+  it('should produce error if input isn\'t number', function() {
+    const badInputs = [true, false, 'cat', function() {}, [1, 2, 3]]
     badInputs.forEach(function(input) {
       (function() {
-        fizzBuzzer(input[0], input[1])
+          fizzBuzzer(input)
       }).should.throw(Error);
     });
   });
